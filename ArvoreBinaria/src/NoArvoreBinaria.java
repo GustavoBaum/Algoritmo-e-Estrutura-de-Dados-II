9df2708fa75b889e9e736e95ddf7e53688400253
GustavoBaum <163304855+GustavoBaum@users.noreply.github.com>
2025-10-08 17:33:30 -0300

    Add files via upload

---
+++ ArvoreBinaria/src/NoArvoreBinaria.java
@@ -0,0 +1,40 @@
+public class NoArvoreBinaria<T> {
+    
+    private T info;
+    private NoArvoreBinaria<T> esquerda;
+    private NoArvoreBinaria<T> direita;
+
+    public NoArvoreBinaria(T info) {
+        setInfo(info);
+    }
+
+    public NoArvoreBinaria(T info, NoArvoreBinaria<T> esq, NoArvoreBinaria<T> dir) {
+        setDireita(dir);
+        setEsquerda(esq);
+        setInfo(info);
+    }
+
+    public void setInfo(T info) {
+        this.info = info;
+    }
+
+    public T getInfo() {
+        return this.info;
+    }
+
+    public void setEsquerda(NoArvoreBinaria<T> esq) {
+        this.esquerda = esq;
+    }
+
+    public NoArvoreBinaria<T> getEsquerda() {
+        return this.esquerda;
+    }
+
+    public void setDireita(NoArvoreBinaria<T> dir) {
+        this.direita = dir;
+    }
+
+    public NoArvoreBinaria<T> getDireita() {
+        return this.direita;
+    }
+}
